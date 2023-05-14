dc6eef4a477498cff37a33621f1f0348c5994271
Ettore Mihaili <ettoremihaili@gmail.com>
2023-05-14 19:35:15 +0200

    test the update feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,35 +4,41 @@
 </script>
 
 <div class="wrapper">
+    <div class="page-heading">
     {#if $page.data.session}
         <h1>{$page.data.session.user?.name} Pupu</h1>
     {:else}
 	<h1>Hei BELA sgnoccola</h1>
     {/if}
-	<p>
+    </div>
+
+    <div class="wrapper">
 		{#if $page.data.session}
 			{#if $page.data.session.user?.image}
 				<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
 			{/if}
-			<span class="signedInText">
+			<p><span class="signedInText">
 				<small>Signed in as</small><br />
 				<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-			</span>
+			</span></p>
 			<button on:click={() => signOut()} class="button">Sign out</button>
 		{:else}
 			<span class="notSignedInText">You are not signed in</span>
 			<button on:click={() => signIn('github')}>Sign In with GitHub</button>
 			<button on:click={() => signIn('google')}>Sign In with Google</button>
 		{/if}
-	</p>
+    </div>	
 </div>
 
 <style>
 	.wrapper {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
 		align-items: center;
 		margin-block: 2rem;
+        gap: 1rem; 
 	}
+    .page-heading {
+        margin-bottom: 2rem; 
+    }
 </style>
