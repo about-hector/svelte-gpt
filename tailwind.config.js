2156e91b4b9bf6c12677cd995aa63cede18beb11
Ettore Mihaili <ettoremihaili@gmail.com>
2023-10-30 12:13:36 +0100

    0.1.2 Minor fixes on the sidebar, created Separator component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+            colors: {
+                border: "hsl(var(--border))",
+            }
+        }
 	},
 	plugins: [require('@tailwindcss/typography')]
 };
