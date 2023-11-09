7a827cea4fee3144ed02ded1b3036bb8b00d2249
Ettore Mihaili <ettoremihaili@gmail.com>
2023-11-09 16:54:53 +0100

    white background was missing on desktop login page

--- src/routes/login/SignIn.svelte
+++ src/routes/login/SignIn.svelte
@@ -2,7 +2,7 @@
 	import SignInCard from './SignInCard.svelte';
 </script>
 
-<div class="hidden md:flex flex-col items-center justify-between text-black w-1/3 p-3 md:static">
+<div class="bg-white hidden md:flex flex-col items-center justify-between text-black w-1/3 p-3 md:static">
 	<h2 class="text-4xl font-bold mt-6">Welcome back!</h2>
     
     <SignInCard />
