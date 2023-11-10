3aa20b1e9f89065cc0ce8370c7bf86a96a567714
Ettore Mihaili <ettoremihaili@gmail.com>
2023-11-10 16:50:52 +0100

    edge problems with prisma, dont know why it runs on ai/chat

--- src/routes/api/ai-chat/+server.ts
+++ src/routes/api/ai-chat/+server.ts
@@ -2,6 +2,7 @@ import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { OPENAI_API_KEY } from '$env/static/private';
 import { SYSTEM_PROMPT } from '$lib/prompt_generators'
+import { json } from '@sveltejs/kit';
 
 // Create an OpenAI API client
 const openai = new OpenAI({
@@ -40,52 +41,14 @@ export async function POST({ request }: { request: Request }) {
         });
 
         // Convert the response into a friendly text-stream
-        const stream = OpenAIStream(response, {
-            onStart: async () => {
-                //save the last prompt to the database
-                //await saveMessage(prompt)
-                //if (!chatId && messages.length <= 1) {
-                //    const chat = await createChat(messages[0], userID);
-                //    const data = await chat.json();
-                //    console.log(data)
-                //    if (data && data.chat.id) {
-                //        chatId = data.chat.id;
-                //    }
-                //    return;
-                //}
-                //const chat = await updateChat(messages[messages.length - 1], userID, chatId)
-                //const data = await chat.json();
-            },
-            onCompletion: async (completion) => {
-                //save the reply received in the database
-                //await saveMessage(answer)
-                //const answer = {
-                //    id: crypto.randomUUID(),
-                //    role: 'assistant',
-                //    createdAt: new Date(),
-                //    content: completion,
-                //    openai_unique_id: crypto.randomUUID()
-                //parent:
-                //}
-                //const chat = await updateChat(answer, userID, chatId)
-            }
-        });
-        //console.log('This message used the ' + model + 'OpenAI model');
-
-        //TODO: process the stream on my own
-        //for await (const part of response) {
-        //console.dir(part, { depth: 3})
-        //process.stdout.write(part.choices[0]?.delta?.content || '');
-        //}
-        //process.stdout.write('\n');
-
-        // Respond with the stream
+        const stream = OpenAIStream(response)
+       // Respond with the stream
         return new StreamingTextResponse(stream);
     } catch (e) {
         // Check if the error is an APIError
         if (e instanceof OpenAI.APIError) {
             const { name, status, headers, message } = e;
-            return Response.json({ name, status, headers, message }, { status });
+            return json({ name, status, headers, message }, { status });
         } else {
             throw e;
         }
